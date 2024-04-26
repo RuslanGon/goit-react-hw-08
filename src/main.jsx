8e8fde20a2008c97d416a63caaf1e7b71435bc22
RuslanGon <ruslan441006@gmail.com>
2024-04-26 15:24:30 +0300

    wer

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import 'modern-normalize'
 import App from './components/App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <HelmetProvider><Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
+    </PersistGate>
     </Provider>
+    </HelmetProvider>
+    
   </React.StrictMode>,
 )
