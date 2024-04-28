68f4d37574061bee618817d10b2cf70193103d0a
RuslanGon <ruslan441006@gmail.com>
2024-04-28 10:25:27 +0300

    wer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,12 +4,13 @@ import css from "./Contact.module.css"
 import { Toaster } from 'react-hot-toast'
 import Modal from "../Modal/Modal"
 import { useState } from "react"
+import ModalChange from "../ModalChange/ModalChange"
 
 
 
 const Contact = ({ contact }) => {
   const [showModal, setShowModal] = useState(false);
-  
+  const [showModalChange, setShowModalChange] = useState(false);
 
   return (<>
     <Toaster position="top-center" reverseOrder={false} />
@@ -17,10 +18,12 @@ const Contact = ({ contact }) => {
           <div className={css.wrapper_contact}>
             <h2><FaUser size="16"/> {contact.name}</h2>
             <p><FaPhone size="16"/> {contact.number}</p>
-          </div>
+      </div>
+      <button type="button" className={css.delete_button} onClick={() => setShowModalChange(true)}>Change</button>
       <button type="button" className={css.delete_button} onClick={() => setShowModal(true)}>Delete</button>
     </li>
     {showModal && <Modal contact={contact} setShowModal={setShowModal} />}
+    {showModalChange && <ModalChange contact={contact} setShowModalChange={setShowModalChange} />}
   </>
   )
 }
