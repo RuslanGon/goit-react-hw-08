bfc83b9bfff22960fe8565db9d230a1995e9d14d
RuslanGon <ruslan441006@gmail.com>
2024-04-28 10:31:59 +0300

    wer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,8 @@
+
 import { Suspense } from 'react'
 import AppBar from '../AppBar/AppBar'
 import Loader from '../Loader/Loader'
+import Footer from '../Footer/Footer'
 
 
 const Layout = ({ children }) => {
@@ -8,9 +10,11 @@ const Layout = ({ children }) => {
     <>
         <AppBar />
             <Suspense fallback={<Loader />}>
-               <main> {children} </main>
-            </Suspense>
-    </>
+          <main> {children} </main>
+        </Suspense>
+       <Footer />
+        </>
+    
     
   )
 }
