67966945c2d62034c500e0d8eb5d447b8327eeab
RuslanGon <ruslan441006@gmail.com>
2024-04-28 10:31:03 +0300

    wer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import css from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={css.footer}>
+          Made by Andriana Petrenko GoIT 2024
+        </footer>
+  )
+}
+
+export default Footer
